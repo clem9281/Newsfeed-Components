ce4ea75a825e5c6740487bb67becd270f1fda910
clem9281 <clem9281@gmail.com>
2019-01-30 18:17:03 -0700

    when the article is marked as read and is moved to the bottom, it now also has a delete button to delete the article entirely

--- Article/Article.js
+++ Article/Article.js
@@ -20,7 +20,7 @@ class Article {
     this.close.textContent = "Mark as Read";
     this.domElement.prepend(this.close);
     this.close.addEventListener("click", () => {
-      this.deleteArticle();
+      this.markAsRead();
     });
 
     // need this stuff for animation
@@ -29,6 +29,11 @@ class Article {
     this.tween = TweenMax.to(this.domElement, 1, {
       height: "400px",
       ease: Back.easeInOut.config(2)
+    }).reverse();
+    // define this tween for later, for marking the article as read
+    this.readTween = TweenMax.to(this.domElement, 1, {
+      opacity: 0,
+      display: "none"
     }).reverse();
   }
 
@@ -48,21 +53,39 @@ class Article {
       this.expandButton.textContent = "Click to Expand";
     }
   }
-  deleteArticle() {
-    let deleteTween = TweenMax.to(this.domElement, 1, {
-      opacity: 0,
-      display: "none"
-    });
+  markAsRead() {
     // i thought it would be fun if the removed article reappeared at the bottom of the articles, but greyed out and the mark as read button removed
-    deleteTween.eventCallback("onComplete", () => {
-      document.querySelector(".articles").appendChild(this.domElement);
-      this.domElement.style.background = "grey";
-      this.close.remove();
-      this.expandButton.setAttribute(
-        "style",
-        "color: black; background: grey;"
-      );
-      deleteTween.reverse();
+    this.readTween.play();
+    this.readTween.eventCallback("onComplete", () =>
+      this.moveElementToBottom()
+    );
+  }
+  moveElementToBottom() {
+    // appendChild moves element from flow, and adds it at the bottom
+    document.querySelector(".articles").appendChild(this.domElement);
+    // set the background to grey: the element is still not visible at this point
+    this.domElement.style.background = "grey";
+    // remove the close (mark as read) button
+    this.close.remove();
+    // put a new button in the place of the old mark as read button: this delete button will delete the article for good
+    this.addDeleteButton();
+    // change the color and background of the expand button
+    this.expandButton.setAttribute("style", "color: black; background: grey;");
+    // reverse the mark as read tween: this line is where the element finally becomes visible at the bottom
+    this.readTween.reverse();
+  }
+  addDeleteButton() {
+    // create new element
+    let deleteButton = document.createElement("p");
+    // style it
+    deleteButton.className = "close";
+    deleteButton.style.color = "black";
+    deleteButton.textContent = "Delete";
+    // add it to the element
+    this.domElement.prepend(deleteButton);
+    // listen for a click event
+    deleteButton.addEventListener("click", () => {
+      this.domElement.remove();
     });
   }
 }
